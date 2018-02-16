b865115d874f22c9cadf5663dbe08973c37210db
acenturyandabit <steeven.liu2@gmail.com>
2018-02-17 08:59:44 +1000

    phone fix 4

--- phono_rdr.js
+++ phono_rdr.js
@@ -1,9 +1,9 @@
 $(document).ready(checkphone);
 function checkphone(){
 	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || window.innerHeight>window.innerWidth){
-		if (!window.location.href.includes("phone"))window.location="phone.html"
+		if (window.location.href.indexOf("phone")==-1)window.location="phone.html"
 	}else{
-		if (!window.location.href.includes("index"))window.location="index.html"
+		if (window.location.href.includes("index")==-1)window.location="index.html"
 	}
 }
 
